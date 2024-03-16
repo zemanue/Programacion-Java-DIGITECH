5b496fa26f96c91229b0280216d9ee8dc12bf267
zemanue <manureco.97@gmail.com>
2024-03-16 13:24:12 +0100

    Update ArraySumasYMedia.java
    Added:
    - Ask the user a row number and gives you the sum of the elements of that row. Also check if the user number is valid (0-3), otherwise asks again.
    - Same with columns

--- ArraySumasYMedia.java
+++ ArraySumasYMedia.java
@@ -29,40 +29,73 @@ public class ArraySumasYMedia {
             }                
         }        
 
+        //Imprimir la matriz de forma que se vean sus filas y columnas
         System.out.println("Valores de la matriz: ");
         for (int i = 0; i < matriz.length; i++) {
             System.out.print("{ ");
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
-            System.out.print("} ");
+            System.out.println("} ");
         }
         System.out.println("");
 
         //Pedir al usuario el número de fila que quiere sumar
+        int sumaFila = 0;
+        int fila;
+        //Bucle do while para preguntar el número de fila y volver a pedir si no se da un número entre 0 y 3.
+        do {
+            System.out.println("¿Qué fila quieres sumar? (Filas: 0, 1, 2 y 3)");
+            fila = sc.nextInt();
+            if (fila < 0 || fila > 3) {
+                System.out.println("El número de fila debe ser entre 0 y 3 (recuerda: 1ª fila = 0, 2ª fila = 1, etc.)");
+            }
+        } while (fila < 0 || fila > 3);
+        //Se suman los elementos de la fila indicada por el usuario
+        for (int i = 0; i < matriz.length; i++) {
+            sumaFila += matriz[fila][i];
+        }
+        
+        System.out.println("Suma de la fila " + fila + " = " + sumaFila);
 
 
-        //Pedir al usuario el número de fila que quiere sumar
 
+        //Pedir al usuario el número de columna que quiere sumar
+        int sumaColumna = 0;
+        int columna;
+        //Bucle do while para preguntar el número de columna y volver a pedir si no se da un número entre 0 y 3.
+        do {
+            System.out.println("¿Qué columna quieres sumar? (Columnas: 0, 1, 2 y 3)");
+            columna = sc.nextInt();
+            if (columna < 0 || columna > 3) {
+                System.out.println("El número de columna debe ser entre 0 y 3 (recuerda: 1ª columna = 0, 2ª columna = 1, etc.)");
+            }
+        } while (columna < 0 || columna > 3);
+        //Se suman los elementos de la columna indicada por el usuario
+        for (int i = 0; i < matriz.length; i++) {
+            sumaColumna += matriz[i][columna];
+        }
+        
+        System.out.println("Suma de la columna " + columna + " = " + sumaColumna);
 
         //Sumar la diagonal principal
         int diagonalPrincipal = matriz [0][0] + matriz [1][1] + matriz [2][2] + matriz [3][3];
-        System.out.println("Suma de la diagonal principal: " + diagonalPrincipal);
+        System.out.println("Suma de la diagonal principal = " + diagonalPrincipal);
 
         //Sumar la diagonal inversa
         int diagonalInversa = matriz [3][0] + matriz [2][1] + matriz [1][2] + matriz [0][3];
-        System.out.println("Suma de la diagonal inversa: " + diagonalInversa);
+        System.out.println("Suma de la diagonal inversa = " + diagonalInversa);
 
         //Media de todos los valores de la matriz
         int sumaMatriz = 0;
         for(int i = 0; i < matriz.length; i++){
             for (int j = 0; j < matriz[i].length; j++) {
-                sumaMatriz = sumaMatriz + matriz[i][j];
+                sumaMatriz += matriz[i][j];
             }
         }
-        double mediaMatriz = (double) sumaMatriz / 16; //ALGO NO ESTÁ BIEN CON LA MEDIA
-        System.out.println("Suma de la matriz: " + sumaMatriz);
-        System.out.println("Media de la matriz: " + mediaMatriz);
+        double mediaMatriz = (double) sumaMatriz / 16;
+        System.out.println("Suma de la matriz = " + sumaMatriz);
+        System.out.println("Media de la matriz = " + mediaMatriz);
 
         sc.close();
     }
