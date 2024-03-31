c2bb52eac722e97a28a48d56de55b47a95082f94
zemanue <manureco.97@gmail.com>
2024-03-31 21:05:50 +0200

    Create ArrayListNumerosAleatorios.java
    Program that stores n random integer numbers between 0 and 100. Then, it displays the values on the list, shows the first and last number, and asks if the user wants to store more values.

---
+++ ArrayList Clase/ArrayListNumerosAleatorios.java
@@ -0,0 +1,59 @@
+/*
+2.Diseña un programa que almacena n números aleatorios, positivos. Se pide:
+ Imprimir todos los valores del arraylist
+ Mostrar cual es el primer valor
+ Mostrar el último valor
+ Se pide por pantalla al usuario si desea almacenar algún valor, si teclea si, se almacena
+el valor tecleado en el arralist.
+ */
+
+import java.util.*;
+
+public class ArrayListNumerosAleatorios {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numeros = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("¿Cuántos números aleatorios quieres almacenar?");
+        int cantidad = sc.nextInt();
+        sc.nextLine(); // Consumir la nueva línea después de nextInt()
+        
+        System.out.println("Generando " + cantidad +  " números enteros aleatorios del 0 al 100.");
+        Random aleatorio = new Random();
+        for (int i = 0; i < cantidad; i++) {
+            numeros.add(aleatorio.nextInt(100));
+        }
+
+        System.out.println("Números aleatorios almacenados: ");
+        System.out.println(numeros);
+        System.out.println("Primer valor: " + numeros.getFirst());
+        System.out.println("Último valor: " + numeros.getLast());
+
+        String siONo;
+        do {
+            System.out.println("Deseas almacenar algún valor más a la lista? (s/n)");
+            siONo = sc.nextLine();
+
+            if (siONo.equalsIgnoreCase("s")) {
+                System.out.println("Muy bien. ¿Cuántos más quieres almacenar?");
+                cantidad = sc.nextInt();
+                sc.nextLine(); // Consumir la nueva línea después de nextInt()
+                System.out.println("Vas a escribir " + " números más. Escríbelos uno por uno:");
+                int masNumeros; 
+                for (int i = 0; i < cantidad; i++) {
+                    masNumeros = sc.nextInt();
+                    numeros.add(masNumeros);
+                }
+                System.out.println("Números almacenados después de los añadidos: ");
+                System.out.println(numeros);
+            } else if (siONo.equalsIgnoreCase("n")) {
+                System.out.println("Muy bien. Lista cerrada.");
+            } else {
+                System.out.println("El carácter introducido no es válido. Por favor, responde 's' para decir que sí o 'n' para decir que no.");
+            }            
+        } while (!siONo.equalsIgnoreCase("s") && !siONo.equalsIgnoreCase("n"));
+
+        sc.close();
+    }
+}
