b931c188eb43ef8f393527b8d217bb8f18e86294
zemanue <manureco.97@gmail.com>
2024-04-01 22:20:06 +0200

    Create ArrayListGestionarListaNumeros.java
    New program that will manage a list of numbers stored both in an Array List and an ordinary array. The instructions for all the cases need to be done (except for case 5: exit).

---
+++ ArrayList Clase/ArrayListGestionarListaNumeros.java
@@ -0,0 +1,72 @@
+/*
+4. Crea un programa en Java para gestionar una lista de números utilizando tanto un ArrayList
+como un array convencional. El programa permitirá realizar operaciones básicas como agregar
+números, calcular la suma y encontrar el número más grande en ambas estructuras.
+ */
+
+import java.util.*;
+
+public class ArrayListGestionarListaNumeros {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arrayListNumeros = new ArrayList<>();
+        int[] arrayNumeros = new int[100]; //Array con un máximo de 100 números.
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+
+        System.out.println("Gestión de lista de números.");
+
+        do {
+            System.out.println("Por favor, pulse uno de los siguientes números para hacer una acción:");
+            System.out.println("- 1: Agregar números.");
+            System.out.println("- 2: Mostrar la lista de números.");
+            System.out.println("- 3: Calcular la suma.");
+            System.out.println("- 4: Encontrar el número más grande");
+            System.out.println("- 5: Salir.");
+
+            opcion = sc.nextInt();
+            int numero;
+
+            switch (opcion) {
+                case 1:     //AGREGAR NÚMEROS
+                    System.out.println("Agregar números.");
+                    //Implementar la funcionalidad para que el usuario pueda elegir a qué estructura quiere añadir números (ArrayList y array convencional).
+
+                break;
+
+
+                case 2:     //MOSTRAR LA LISTA DE NÚMEROS
+                System.out.println("Mostrar la lista de números.");
+
+                break;
+
+
+                case 3:     //CALCULAR LA SUMA
+                System.out.println("Calcular la suma.");
+                //Se suman los números en ambas estructuras.
+
+                break;
+
+
+                case 4:     //ENCONTRAR EL NÚMERO MÁS GRANDE
+                System.out.println("Encontrar el número más grande.");
+                //Hacerlo en ambas estructuras
+
+                break;
+
+
+                case 5:     //SALIR
+                System.out.println("Saliendo del programa.");
+                break;
+                
+                default://Si la respuesta es diferente a los números especificados, repite el bucle.
+                System.out.println("El número introducido no es válido.");
+                    break;
+            }
+
+        } while (opcion != 5);
+
+        sc.close();
+    }
+}
