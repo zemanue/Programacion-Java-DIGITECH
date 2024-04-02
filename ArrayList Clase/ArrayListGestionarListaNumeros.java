47d5cb6b4f1a5e5151bc42acebe1f67ecccd13b7
zemanue <manureco.97@gmail.com>
2024-04-02 23:55:05 +0200

    Update ArrayListGestionarListaNumeros.java
    Added case 1 and 2 functionalities:
    - Case 1: adds integers to the array or the ArrayList. User can specify how many numbers are going to be registered and typed them one by one. The array has a defined capacity of 30 integers.
    - Case 2: show all the values of both the array and ArrayList.

--- ArrayList Clase/ArrayListGestionarListaNumeros.java
+++ ArrayList Clase/ArrayListGestionarListaNumeros.java
@@ -10,7 +10,7 @@ public class ArrayListGestionarListaNumeros {
     public static void main(String[] args) {
         
         ArrayList<Integer> arrayListNumeros = new ArrayList<>();
-        int[] arrayNumeros = new int[100]; //Array con un máximo de 100 números.
+        int[] arrayNumeros = new int[30]; //Array con un máximo de 30 números.
         Scanner sc = new Scanner(System.in);
 
         int opcion;
@@ -26,38 +26,66 @@ public class ArrayListGestionarListaNumeros {
             System.out.println("- 5: Salir.");
 
             opcion = sc.nextInt();
-            int numero;
+            int numero, cantidad;
 
             switch (opcion) {
                 case 1:     //AGREGAR NÚMEROS
                     System.out.println("Agregar números.");
-                    String arrayOArrayList;
+                    int arrayOArrayList;
                     do {
                         System.out.println("Escribe '1' para agregar números en el array, o escribe '2' para agregarlos en el ArrayList.");
-                        arrayOArrayList = sc.nextLine();
+                        arrayOArrayList = sc.nextInt();
                         switch (arrayOArrayList) {
-                            case "1":
-                                
+
+                            case 1: //Añadir al array
+                                System.out.println("Añadiendo al array convencional. Si había números registrados, se restablecerán. ¿Cuántos números quieres añadir?");
+                                System.out.println("Nota: el array tiene un tamaño máximo de " + arrayNumeros.length + " números. Si quieres añadir menos números, el resto se rellenarán con ceros.");
+                                cantidad = sc.nextInt();
+                                if (cantidad > arrayNumeros.length) {
+                                    System.out.println("No se pueden registrar " + cantidad + " números porque el array solo puede almacenar un máximo de " + arrayNumeros.length + " números.");
+                                    System.out.println("Volviendo al inicio.");
+                                } else {
+                                    System.out.println("Muy bien. Escribe uno a uno los números a añadir:");
+                                for (int i = 0; i < cantidad; i++) {
+                                    numero = sc.nextInt();
+                                    arrayNumeros[i] = numero;
+                                }
+                                System.out.println("Números registrados. Volviendo al inicio.");                  
+                                }                                              
                                 break;
                         
-                            case "2":
-                                
+                            case 2: //Añadir al ArrayList
+                                System.out.println("Añadiendo al ArrayList. ¿Cuántos números quieres añadir?");
+                                cantidad = sc.nextInt();
+                                System.out.println("Muy bien. Escribe uno a uno los números a añadir:");
+                                for (int i = 0; i < cantidad; i++) {
+                                    numero = sc.nextInt();
+                                    arrayListNumeros.add(numero);
+                                }
+                                System.out.println("Números registrados. Volviendo al inicio.");                                
                             break;
     
                             default:
                             System.out.println("El número introducido no es válido.");
                                 break;
                         }
-                    } while (!arrayOArrayList.equalsIgnoreCase("salir"));
-
-
-                    //Implementar la funcionalidad para que el usuario pueda elegir a qué estructura quiere añadir números (ArrayList y array convencional).
-
+                    } while (arrayOArrayList != 1 && arrayOArrayList != 2);
                 break;
 
 
                 case 2:     //MOSTRAR LA LISTA DE NÚMEROS
                 System.out.println("Mostrar la lista de números.");
+                System.out.print("Valores del array: ");
+                System.out.print("{"); //Algoritmo para imprimir el array entre llaves y comas
+                for (int i = 0; i < arrayNumeros.length; i++) { 
+                    if (i+1 != arrayNumeros.length) {   
+                        System.out.print(arrayNumeros[i] + ", ");
+                    } else {
+                        System.out.println(arrayNumeros[i] + "}");
+                    }
+                }
+                System.out.println("Nota: todos los números que no han sido rellenados por el usuario se presentan como ceros.");
+                System.out.println("Valores del ArrayList: " +  arrayListNumeros);
 
                 break;
 
