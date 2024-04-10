14e8b3fe031ba8417c9730a2710cf305986d3e2c
zemanue <manureco.97@gmail.com>
2024-04-10 17:01:33 +0200

    Create POOMetodosOperacionesMatematicas.java
    Structure created, no functionality yet.

---
+++ POO - Programacion Orientada a Objetos/POOMetodosOperacionesMatematicas.java
@@ -0,0 +1,50 @@
+/*
+Realizar un programa calculadora que tendrá las siguientes características:
+Realiza menú con las siguientes opciones:
+1. Sumar
+2. Restar
+3. Multiplicar
+4. Dividir
+5. Salir.
+Vamos a tener los siguientes métodos:
+- pedirNumero.(pediremos un número y me devolverá dicho número)
+- sumar.(introduciremos los dos números como parámetros y me devolverá la suma).
+- restar.(introduciremos los dos números como parámetros y me devolverá la resta).
+- multiplicar.(introduciremos los dos números como parámetros y me devolverá la multiplicación).
+- dividir.(introduciremos los dos números como parámetros y me devolverá la división).
+- En el main tendremos el menú y las llamadas a los métodos, imprimiremos por
+pantalla los resultados de los métodos anteriores, 
+ */
+
+public class OperacionesMatematicas {
+    public int pedirNumero() {
+        
+    }
+    
+    public int sumar(num1, num2) {
+        int suma = num1 + num2;
+        return suma;
+    }
+    
+    public int restar(num1, num2) {
+        int resta = num1 - num2;
+        return resta;
+    }
+
+    public int multiplicar(num1, num2) {
+        int multiplicacion = num1 * num2;
+        return multiplicacion;
+    }
+
+    public int dividir(num1, num2) {
+        int division = num1 / num2;
+        return division;
+    }
+}
+
+public class POOMetodosOperacionesMatematicas {
+    public static void main(String[] args) {
+
+        
+    }
+}
