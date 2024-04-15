5382035d055a9dfafc2238fdf7c2d4aeda697a45
zemanue <manureco.97@gmail.com>
2024-04-15 13:33:35 +0200

    Update POOMostrarArray.java
    - An array is created and filled with random numbers from 1 to 10. Then, "Vector" methods are used.
    - "Vector" methods fixed (mostrarAlReves was not designed properly).

--- POO - Programacion Orientada a Objetos/POOMostrarArray.java
+++ POO - Programacion Orientada a Objetos/POOMostrarArray.java
@@ -3,41 +3,50 @@
 Vamos a tener dos métodos uno para mostrarlo al derecho y otro para mostrarlo al revés.
  */
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class POOMostrarArray {
     
-    //int[] array = new int[10];
-
-    class Vector {
+    static class Vector {
         void mostrarAlDerecho(int[] array) {
             System.out.println("Impresión al derecho de todos los valores del vector:");
+            //Imprimir los valores entre corchetes y separados por comas
             System.out.print("{");
             for (int i = 0; i < array.length; i++) {
-                if (i+1 != array.length) {
-                    System.out.print(array[i] + ", ");
-                } else {
-                    System.out.println(array[i] + "}");
+                if (i != 0) {
+                    System.out.print(", ");
                 }
+                System.out.print(array[i]);
             }
+            System.out.println("}");
         }
 
         void mostrarAlReves(int[] array) {
-            System.out.println("Impresión al derecho de todos los valores del vector:");
+            System.out.println("Impresión al revés de todos los valores del vector:");
+            //Imprimir los valores entre corchetes y separados por comas
             System.out.print("{");
-            for (int i = array.length; i >= 0; i--) {
-                if (i+1 != array.length) {
-                    System.out.print(array[i] + ", ");
-                } else {
-                    System.out.println(array[i] + "}");
+            for (int i = array.length - 1; i >= 0; i--) {
+                if (i != array.length - 1) {
+                    System.out.print(", ");
                 }
+                System.out.print(array[i]);
             }
+            System.out.println("}");
         }
     }
 
     public static void main(String[] args) {
         
-
-
+        Vector vector1 = new Vector();
+        // Crear y llenar el vector con números aleatorios del 1 al 10
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10) + 1; // Genera un número aleatorio entre 1 y 10
+        }
+        System.out.println("Se ha rellenado el vector con " + array.length + " números aleatorios del 1 al 10.");
+        vector1.mostrarAlDerecho(array);
+        vector1.mostrarAlReves(array);
+        
     }
 }
