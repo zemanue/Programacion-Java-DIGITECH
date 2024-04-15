77693c58b78390e97182fa8cce7c40d1468f5ba6
zemanue <manureco.97@gmail.com>
2024-04-15 21:28:09 +0200

    Update POOPersona.java
    - "AtributosPersona" class renamed to "Persona"
    - Getters and setters for the private variables
    - Method cargarDatos() to use the setters
    - Method mostrarDatos() to use the getters
    - Method cambiarDatos() to use the setters again.
    - The main method creates "persona1", instance of the class "Persona" and uses all the methods.

--- POO - Programacion Orientada a Objetos/POOPersona.java
+++ POO - Programacion Orientada a Objetos/POOPersona.java
@@ -1,6 +1,5 @@
 /*
- * 4. Tenemos una persona que tiene las propiedades: nombre, apellido, calle, teléfono,
-aficiones.
+ * 4. Tenemos una persona que tiene las propiedades: nombre, apellido, calle, teléfono, aficiones.
 Vamos a crear 3 clases.
 - 1ª clase: 
 Vamos a tener los atributos privados de la clase de persona.
@@ -15,37 +14,87 @@ los visualizaremos y si se ha producido alguna modificación se vuelve a mostrar
  */
 
 
-public class POOPersona {
-    
-    public class AtributosPersona {
-        private String nombre;
-        private String apellido;
-        private String calle;
-        private String telefono;
-        private String aficiones;
-
-
-    public class MetodosPersona {
-        void cargarDatos() {
-
+    class Persona {
+        private String nombre, apellido, calle, telefono;
+        private String[] aficiones = new String[3];
+        
+        //Getters
+        public String getNombre() {
+            return nombre;
+        }
+        public String getApellido() {
+            return apellido;
+        }       
+        public String getCalle() {
+            return calle;
+        }
+        public String getTelefono() {
+            return telefono;
+        }
+        public String[] getAficiones() {
+            return aficiones;
         }
 
-        void mostrarDatos() {
-
+        //Setters
+        public void setNombre(String suNombre) {
+            this.nombre = suNombre;
         }
-
-        void cambiarDatos() {
-
+        public void setApellido(String suApellido) {
+            this.apellido = suApellido;
+        }
+        public void setCalle(String suCalle) {
+            this.calle = suCalle;
+        }
+        public void setTelefono(String suTelefono) {
+            this.telefono = suTelefono;
+        }
+        public void setAficiones(String[] susAficiones) {
+            this.aficiones = susAficiones;
         }
     }
+
+    class MetodosPersona {
+        static void cargarDatos(Persona persona) {
+            persona.setNombre("Juan");
+            persona.setApellido("Perez");
+            persona.setCalle("Calle Diablo");
+            persona.setTelefono("677776776");
+            String[] susAficiones = {"Deporte", "Leer", "Viajar"};
+            persona.setAficiones(susAficiones);
+        }
+
+        static void mostrarDatos(Persona persona) {
+            System.out.println("Nombre: " + persona.getNombre());  
+            System.out.println("Apellido: " + persona.getApellido());  
+            System.out.println("Calle: " + persona.getCalle());  
+            System.out.println("Teléfono: " + persona.getTelefono());  
+            System.out.println("Aficiones: "); 
+            for (String susAficiones : persona.getAficiones()) {
+                System.out.println("- " + susAficiones);
+            }
+        }
+
+        static void cambiarDatos(Persona Persona, String suNombre, String suApellido, String suCalle, String suTelefono, String[] susAficiones) {
+            Persona.setNombre(suNombre);
+            Persona.setApellido(suApellido);
+            Persona.setCalle(suCalle);
+            Persona.setTelefono(suTelefono);
+            Persona.setAficiones(susAficiones);
+        }
+    }
+public class POOPersona {
 
     public static void main(String[] args) {
-        POOPersona persona1 = new POOPersona();
+        Persona persona1 = new Persona();
 
+        MetodosPersona.cargarDatos(persona1);
 
-    }
+        MetodosPersona.mostrarDatos(persona1);
 
+        String[] nuevasAficiones = {"Montar a caballo", "Esquiar", "Programar"};
+        MetodosPersona.cambiarDatos(persona1, "Ana", "Lopez", "Calle Angel", "611223344", nuevasAficiones);
 
+        MetodosPersona.mostrarDatos(persona1);
 
     }
 }
