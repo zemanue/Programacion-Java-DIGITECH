5d0a00a206a0f1a018b045aaecedf4aacc84aa07
zemanue <manureco.97@gmail.com>
2024-04-15 19:50:08 +0200

    Create POOPersona.java
    Structure created:
    - Class "AtributosPersona", were 5 private variables are stored (name, surname, street, telephone number, hobbies).
    - Class "MetodosPersona", with three methods (load data, show data and change data).
    - Main class, with an object created.

---
+++ POO - Programacion Orientada a Objetos/POOPersona.java
@@ -0,0 +1,51 @@
+/*
+ * 4. Tenemos una persona que tiene las propiedades: nombre, apellido, calle, teléfono,
+aficiones.
+Vamos a crear 3 clases.
+- 1ª clase: 
+Vamos a tener los atributos privados de la clase de persona.
+- 2ª clase: 
+Vamos a tener los métodos:
+    -cargarDatos(): Vamos a asignar valores a los atributos. Para el atributo aficiones, tendremos 3 aficiones.
+    -mostrarDatos().
+    -cambiarDatos().
+- 3ª clase:
+Vamos a tener el método principal que llame a los métodos de la 2ª clase. Vamos a cargar los datos, 
+los visualizaremos y si se ha producido alguna modificación se vuelve a mostrar todos los datos.
+ */
+
+
+public class POOPersona {
+    
+    public class AtributosPersona {
+        private String nombre;
+        private String apellido;
+        private String calle;
+        private String telefono;
+        private String aficiones;
+
+
+    public class MetodosPersona {
+        void cargarDatos() {
+
+        }
+
+        void mostrarDatos() {
+
+        }
+
+        void cambiarDatos() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        POOPersona persona1 = new POOPersona();
+
+
+    }
+
+
+
+    }
+}
