9ecf320492acef3f8b51927d9c469a387caa14cb
zemanue <manureco.97@gmail.com>
2024-04-16 12:59:55 +0200

    Update POOActoresFamosos.java
    - modificarNombres() done: allows the user to specify the position of the name they want to change and write the updated name. The user can also write "no" if they want to try another position, or write "salir" if they want to cancel and go back to the menu.
    - If the user wants to use modificarNombres(), borrarNombres(), buscarNombre(), visualizarNombres() or visualizarCantantes() methods and the ArrayList is empty, the method sends them back to the menu.

--- POO - Programacion Orientada a Objetos/POOActoresFamosos.java
+++ POO - Programacion Orientada a Objetos/POOActoresFamosos.java
@@ -25,9 +25,9 @@ import java.util.ArrayList;
 
 class ListaDeActores {
 
-    ArrayList<String> listaActores = new ArrayList<>(); 
+    ArrayList<String> listaActores = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-    String nombreActor;
+    String nombreActor = "0", respuesta = "0"; //Evita errores al poner estas variables como condición en los bucles
     int numeroNombres, posicion;
 
     public void agregarNombres() {
@@ -39,39 +39,84 @@ class ListaDeActores {
         for (int i = 0; i < numeroNombres; i++) {
             nombreActor = sc.nextLine();
             if (nombreActor.equalsIgnoreCase("salir")) {
-                System.out.println("Volviendo al inicio.");
+                System.out.println("Saliendo al menú.");
                 break;
             } else {
                 listaActores.add(nombreActor);
                 System.out.println("Nombre registrado.");
             }
         }
-        System.out.println("Nuevos nombres agregados. Volvemos al menú.");
+        if (!nombreActor.equalsIgnoreCase("salir")) {
+            System.out.println("Nuevos nombres agregados. Volvemos al menú.");
+        }        
     }
 
-/*     public void modificarNombres() {
-
+    public void modificarNombres() {
+        if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al menú.
+            System.out.println("No hay ningún nombre registrado en la lista. Escriba '1' para agregar algún nombre primero.");
+        } else {
+            boolean repetir;
+            System.out.println("Modificar un nombre.");
+            do {
+                repetir = false;
+                System.out.println("La lista tiene actualmente " + listaActores.size() + " posiciones. Escriba la posición en la que está registrado el nombre que desea modificar (1, 2, 3, 4...):");
+                posicion = sc.nextInt() - 1;
+                sc.nextLine(); // Consumir la nueva línea después de nextInt()
+                if (posicion+1 > listaActores.size() || posicion+1 <= 0) {
+                    System.out.println("La posición indicada no existe en la lista actualmente. Debe ser un número entre 1 y " + listaActores.size() + ".");
+                    repetir = true;
+                } else {                                        
+                    System.out.println("El nombre asignado en esa posición es: " + listaActores.get(posicion));
+        
+                    System.out.println("Escriba ahora por qué nombre lo quiere modificar.");
+                    System.out.println("¿No es el nombre que buscaba? Escriba 'no' para escribir otra posición, o 'salir' si quiere cancelar la operación y volver al inicio.");
+                    respuesta = sc.nextLine();
+                    // if else para posibles respuestas: 'no', 'salir' u otras combinaciones para modificar el nombre.
+                    if (respuesta.equalsIgnoreCase("no")) { //Si el usuario teclea 'no', se vuelve a pedir la posición.
+                        System.out.println("Buscando otro nombre de la lista.");
+                        repetir = true;
+                    } else if (respuesta.equalsIgnoreCase("salir")) { //Si se escribe 'salir', se sale de nuevo al menú
+                        System.out.println("Volviendo al menú.");
+                    } else { //Si teclea cualquier valor que no sea 'n' o 'salir', se registra el nombre.
+                        listaActores.set(posicion, respuesta);
+                        System.out.println("Nombre modificado.");
+                    }
+                }
+            } while (repetir);
+        }
     }
 
-    public void borrarNombres() {
+/*    public void borrarNombres() {
+        if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al menú.
+            System.out.println("No hay ningún nombre registrado en la lista. Escriba '1' para agregar algún nombre primero.");
+        } else {
 
+        }
     }
 
     public String buscarNombre() {
-        return ;
+                if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al menú.
+            System.out.println("No hay ningún nombre registrado en la lista. Escriba '1' para agregar algún nombre primero.");
+        } else {
+            return ;
+        }
     }
 */
     public void visualizarNombres() {
-        if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al inicio.
-            System.out.println("No hay ningún empleado registrado en la lista. Escriba '1' para agregar algún nombre primero.");
+        if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al menú.
+            System.out.println("No hay ningún nombre registrado en la lista. Escriba '1' para agregar algún nombre primero.");
         } else {
-            System.out.println("Visualizar nombres. Estos son los nombres que están registrados en la lista: ");
+            System.out.println("Visualizar nombres. Estos son los nombres que están registrados en la lista actualmente: ");
             System.out.println(listaActores);
         }
     }
 
 /*    public void visualizarCantantes() {
-
+        if (listaActores.isEmpty()) { //Si la lista está vacía, enviar de nuevo al menú.
+            System.out.println("No hay ningún nombre registrado en la lista. Escriba '1' para agregar algún nombre primero.");
+        } else {
+            
+        }
     }*/
 
     public void salir(){
@@ -102,22 +147,22 @@ public class POOActoresFamosos {
                 listaActores.agregarNombres();
                 break;
             case 2:
-                //modificarNombres()
+                listaActores.modificarNombres();
                 break;   
             case 3:
-                //borrarNombres()
+                //listaActores.borrarNombres();
                 break;  
             case 4:
-                //buscarNombre()
+                //listaActores.buscarNombre();
                 break; 
             case 5:
                 listaActores.visualizarNombres();
                 break; 
             case 6:
-                //visualizarCantantes()
+                //listaActores.visualizarCantantes();
                 break;
             case 7:
-                //salir()
+                listaActores.salir();
                 break;
             default:
             System.out.println("La opción elegida no es válida. Volviendo al inicio.");
