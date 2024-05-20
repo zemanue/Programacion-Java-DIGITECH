fad88f63da45139f1b99a4bb51daf3a4939a6420
zemanue <manureco.97@gmail.com>
2024-05-20 11:19:23 +0200

    Create Persona.java
    "Persona" class that stores private variables like the name, surnames and age of a person, with its getters and setters, a few constructors and a print data method (imprimirDatos())

---
+++ POO - Programacion Orientada a Objetos/Tarea9Herencia/Persona.java
@@ -0,0 +1,58 @@
+package Tarea9Herencia;
+
+public class Persona {
+
+    // Variables privadas
+    private String nombre, apellidos;
+    private int edad;
+    
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }       
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setters
+    public void setNombre(String suNombre) {
+        this.nombre = suNombre;
+    }
+    public void setApellidos(String suApellido) {
+        this.apellidos = suApellido;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Método para imprimir todos los datos
+    public void imprimirDatos(Persona persona) {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellidos: " + getApellidos());
+        System.out.println("Edad: " + getEdad());
+    }
+
+    //Constructores
+    public Persona(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public Persona(String nombre, String apellidos) {
+        this.nombre = nombre;
+    }
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    public Persona(int edad) {
+        this.edad = edad;
+    }
+    public Persona(String apellidos, int edad) {
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+}
+    
