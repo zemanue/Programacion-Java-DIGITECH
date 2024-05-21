e4020d4a3416a9bb6d7f50069d607616e1017b77
zemanue <manureco.97@gmail.com>
2024-05-21 12:53:25 +0200

    Create FileLeerArchivo and a needed .txt
    Create a program that reads a text file caracter by caracter and shows its content without whitespaces. It also catchs a few exceptions.

---
+++ POO - Programacion Orientada a Objetos/Excepciones/FileLeerArchivo.java
@@ -0,0 +1,40 @@
+/*
+Práctica Archivos I
+Crea un fichero de texto con el nombre y contenido que tu quieras. Ahora crea una aplicación
+que lea este fichero de texto carácter a carácter y muestre su contenido por pantalla sin
+espacios.
+Por ejemplo, si un fichero tiene el siguiente texto “Esto es una prueba”, deberá mostrar
+“Estoesunaprueba”.
+ */
+
+package Excepciones;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileLeerArchivo {
+
+public static void main(String[] args) {
+    File archivo = new File("fichero-de-texto.txt");
+    FileReader lector = null;
+    try {
+        lector = new FileReader(archivo);
+        try {
+            int caracter;
+            while ((caracter = lector.read()) != -1) {
+                if (!Character.isWhitespace((char) caracter)) {
+                    System.out.print((char) caracter);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error inesperado: " + e.getMessage());
+        } finally {
+            lector.close();
+        }
+
+    } catch (IOException e) {
+        System.err.println("Error al leer el archivo: " + e.getMessage());
+    }
+}
+}
