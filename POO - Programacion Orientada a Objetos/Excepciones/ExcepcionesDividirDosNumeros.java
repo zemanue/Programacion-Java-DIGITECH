bea5a038a93e24fe8766787f5be131cf794b997a
zemanue <manureco.97@gmail.com>
2024-05-21 12:02:45 +0200

    Update ExcepcionesDividirDosNumeros.java
    Add exceptions:
    - IllegalArgumentException if the first number isn't between 1-99 or the second number == 0.
    - InputMismatchException: if the input is not a number.

--- POO - Programacion Orientada a Objetos/Excepciones/ExcepcionesDividirDosNumeros.java
+++ POO - Programacion Orientada a Objetos/Excepciones/ExcepcionesDividirDosNumeros.java
@@ -2,11 +2,12 @@
 1.Diseña un programa que divide dos números. El código tiene que contemplar:
 - Que el segundo número no sea cero
 - Que el primer número no sea cero ni menor que cero
-- Que el primer valor sea mayor que 100
+- Que el primer valor sea menor que 100
 */
 
 package Excepciones;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExcepcionesDividirDosNumeros {
@@ -15,16 +16,27 @@ public class ExcepcionesDividirDosNumeros {
         
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Inserte a continuación dos números para dividirlos.");
-        System.out.print("Dividendo: ");
-        double dividendo = sc.nextDouble();
+        try {
+            System.out.println("Inserte a continuación dos números para dividirlos.");
+            System.out.print("Dividendo (debe ser mayor que 0 y menor que 100): ");
+            double dividendo = sc.nextDouble();
+            if (dividendo <= 0 || dividendo >= 100) {
+                throw new IllegalArgumentException("El número debe ser mayor que 0 y menor que 100.");
+            }
 
-        System.out.print("Divisor: ");
-        double divisor = sc.nextDouble();
+            System.out.print("Divisor: ");
+            double divisor = sc.nextDouble();
+            if (divisor == 0) {
+                throw new IllegalArgumentException("Error: no se puede dividir entre 0.");
+            }
 
-        double cociente = dividendo / divisor;
-        System.out.println(dividendo + " / " + divisor + " = " + cociente);
+            double cociente = dividendo / divisor;
+            System.out.println("Resultado: " + dividendo + " / " + divisor + " = " + cociente);
 
-        sc.close();
+        } catch (InputMismatchException e) {
+            System.err.println("Error: Se debe introducir un número entero o decimal.");
+        } finally {
+            sc.close();
+        }
     }
 }
